ce8658b8c7c72936d5603ef29d2a4c8f61e49acf
Hayes <hem226@gmail.com>
2016-10-06 14:09:19 +0800

    Mod12 Delegate

--- Mod11/Program.cs
+++ Mod11/Program.cs
@@ -28,29 +28,31 @@ namespace Mod11
 
 
 
-            try
-            {
-                //程式邏輯
-                string str = Console.ReadLine();
-                int.Parse(str);
-            }
-            catch (System.OverflowException e)
-            {
-                Console.WriteLine(e.Message + " 數字太大或太小");
-            }
-            catch (Exception ex)
-            {
-                //錯誤處理邏輯
-                //Console.WriteLine("Error! Sorry!");
+            //try
+            //{
+            //    //程式邏輯
+            //    string str = Console.ReadLine();
+            //    int.Parse(str);
+            //}
+            //catch (System.OverflowException)
+            //{
+            //    Console.WriteLine("數字太大或太小");
+            //}
+            //catch (Exception ex)
+            //{
+            //    //錯誤處理邏輯
+            //    //Console.WriteLine("Error! Sorry!");
 
-                //Console.WriteLine(ex.Message);
-                //Console.WriteLine(ex.StackTrace);
-                Console.WriteLine(ex.GetType());
-            }
-            finally
-            {
-                Console.WriteLine("結束");
-            }
+            //    //Console.WriteLine(ex.Message);
+            //    //Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine(ex.GetType());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("結束");
+            //}
+
+            throw new Exception("AAAAAAAAAAAAAAAAAAAAAA");
 
         }
 
---
+++ Mod12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHelloDelegate d = new SayHelloDelegate(SayHello);
+            d.Invoke("tony");
+
+        }
+
+        delegate void SayHelloDelegate(string name);
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
